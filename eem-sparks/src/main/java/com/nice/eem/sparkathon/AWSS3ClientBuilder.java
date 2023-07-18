46d25b86d47d586a858e9687a1e40df229fceeac
pbibek <bibek.poudel@nice.com>
2023-07-18 15:20:21 +0530

    initial commit

--- eem-sparks/src/main/java/com/nice/eem/sparkathon/AWSS3ClientBuilder.java
+++ eem-sparks/src/main/java/com/nice/eem/sparkathon/AWSS3ClientBuilder.java
@@ -1,23 +1,17 @@
 package com.nice.eem.sparkathon;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.stereotype.Component;
-
-
-
-@Component
-@Slf4j
 public class AWSS3ClientBuilder {
 
 
+    public static void main(String[] args) {
+        buildS3Client();
+    }
 
 
     /**
@@ -25,29 +19,17 @@ public class AWSS3ClientBuilder {
      *
      * @return
      */
-    public AmazonS3 buildS3Client(S3Config s3Config) {
-        AWSCredentials awsCredentials;
+    public static AmazonS3 buildS3Client() {
         AmazonS3 s3Client;
-        String cloudDeployment = s3Config.getIsCloudDeployment();
-        String awsRegion = s3Config.getRegion();
-        if (cloudDeployment.equalsIgnoreCase("false")) {
-            log.info("Building aws s3 client for non-cloud deployment, isCloudDeployment = {}, aws region = {}", cloudDeployment, awsRegion);
-            String accessKey = s3Config.getAccessKey();
-            String secretKey = s3Config.getSecretKey();
-            awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-            s3Client = AmazonS3ClientBuilder
-                    .standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                    .withRegion(awsRegion)
-                    .build();
-        } else {
-            log.info("Building aws s3 client for clould deployment, isCloudDeployment = {}, aws region = {}", cloudDeployment, awsRegion);
-            s3Client = AmazonS3ClientBuilder
-                    .standard()
-                    .withCredentials(new InstanceProfileCredentialsProvider(true))
-                    .withRegion(awsRegion)
-                    .build();
-        }
+        String awsRegion = "us-east-1";
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("XXXXXXXXXXXXXXXXXXXX", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+
+        s3Client = AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(awsRegion)
+                .build();
+
         return s3Client;
     }
 
