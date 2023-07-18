c6635fbbcdae013cc76036f719e9c5ec6ec46baf
pbibek <bibek.poudel@nice.com>
2023-07-18 19:34:15 +0530

    initial commit

---
+++ eem-sparks/src/main/java/com/nice/eem/sparkathon/QueryConstants.java
@@ -0,0 +1,11 @@
+package com.nice.eem.sparkathon;
+
+public class QueryConstants {
+
+
+    //sql query to fetch agentdetails by agentid
+    public static final String AGENT_DETAILS_BY_AGENTID = "select * from agent_details where agentid = ?";
+
+
+
+}
